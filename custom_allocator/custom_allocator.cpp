0e043ff29fc4ae86d15eb082345b50ff4f21b81c
Bigblue <shenpaiwilliam@gmail.com>
2021-12-27 21:47:30 -0800

    Add project files.

---
+++ custom_allocator/custom_allocator.cpp
@@ -0,0 +1,12 @@
+﻿// custom_allocator.cpp : Defines the entry point for the application.
+//
+
+#include "custom_allocator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
